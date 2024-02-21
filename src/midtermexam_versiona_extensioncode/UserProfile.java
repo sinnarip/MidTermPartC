0edc294a10dd1abf20f4ec5973fb0024e4a9ea4a
sinnarip <159319205+sinnarip@users.noreply.github.com>
2024-02-21 15:07:42 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -2,6 +2,11 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
+
+Name: Naripjeet Singh
+Student ID: 991717845
+ 
  */
 package midtermexam_versiona_extensioncode;
 
